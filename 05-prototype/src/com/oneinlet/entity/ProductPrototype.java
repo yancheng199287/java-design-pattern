2cc304ad62319521f1585ef6f8238222180728c9
yancheng <648830605@qq.com>
2018-06-04 10:32:29 +0800

    1. 新增 原型设计模式

---
+++ 05-prototype/src/com/oneinlet/entity/ProductPrototype.java
@@ -0,0 +1,8 @@
+package com.oneinlet.entity;
+
+public class ProductPrototype extends Prototype {
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ 05-prototype/src/com/oneinlet/entity/Prototype.java
@@ -0,0 +1,9 @@
+package com.oneinlet.entity;
+
+public class Prototype implements Cloneable {
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
