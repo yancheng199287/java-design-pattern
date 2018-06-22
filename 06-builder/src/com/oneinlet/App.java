2bd9d18908b8437c0ffe8bc62720903a9e6f0d1b
yancheng <648830605@qq.com>
2018-06-22 09:43:40 +0800

    1. 新增 建造者设计模式

--- 06-builder/src/com/oneinlet/App.java
+++ 06-builder/src/com/oneinlet/App.java
@@ -1,10 +1,16 @@
 package com.oneinlet;
 
 /**
- * 抽象工厂模式
+ * 建造者模式
+ * 一、什么是建造者模式
+ * 建造者模式：是将一个复杂的对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。
+ * 方便用户创建复杂的对象（不需要知道实现过程）
+ * 代码复用性 & 封装性（将对象构建过程和细节进行封装 & 复用）
  */
 public class App {
     public static void main(String[] args) {
-
+        CarDirector carDirector = new CarDirector();
+        Car car = carDirector.CarDirector(new CarConcreteBuilder());
+        System.out.println(car.toString());
     }
 }
