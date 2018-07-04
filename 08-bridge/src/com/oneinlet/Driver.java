b9d69ac3490f3eeb73fd95f606a54b2823b5ebe0
yancheng <648830605@qq.com>
2018-07-04 11:21:39 +0800

    1. 新增 桥接设计模式

---
+++ 08-bridge/src/com/oneinlet/Driver.java
@@ -0,0 +1,5 @@
+package com.oneinlet;
+
+public interface Driver {
+    void connect(String host, int port, String user, String password);
+}
---
+++ 08-bridge/src/com/oneinlet/MysqlDriver.java
@@ -0,0 +1,13 @@
+package com.oneinlet;
+
+/**
+ * 具体实现
+ * 针对不同的驱动接口，会有多种实现，在这里实现mysql数据库类型的驱动
+ */
+public class MysqlDriver implements Driver {
+    @Override
+    public void connect(String host, int port, String user, String password) {
+        String s = String.format("进行mysql数据库连接，主机名:%s, 端口:%s, 用户名:%s, 密码:%s", host, port, user, password);
+        System.out.println(s);
+    }
+}
