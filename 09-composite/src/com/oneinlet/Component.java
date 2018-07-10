d77270c7f989b8bfc0e4a47ed0d70759b88710c9
yancheng <648830605@qq.com>
2018-07-10 10:56:42 +0800

    1. 新增 组合模式

---
+++ 09-composite/src/com/oneinlet/Component.java
@@ -0,0 +1,9 @@
+package com.oneinlet;
+
+// 抽象构建
+public abstract class Component {
+    //个体和整体都具有的共享
+    public void doSomething() {
+        //编写业务逻辑
+    }
+}
---
+++ 09-composite/src/com/oneinlet/Leaf.java
@@ -0,0 +1,11 @@
+package com.oneinlet;
+
+
+// 树叶构件
+public class Leaf extends Component {
+
+    @Override
+    public void doSomething() {
+        System.out.println("叶子终节点...");
+    }
+}
