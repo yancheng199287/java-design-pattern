01e5e15c5e50ce864402adc44e08223de504fdb7
hexiaoming123 <17710146600@163.com>
2019-07-20 22:24:26 +0800

    新增 享元模式

--- 12-flyweight/src/com/oneinlet/App.java
+++ 12-flyweight/src/com/oneinlet/App.java
@@ -1,7 +1,7 @@
 package com.oneinlet;
 
 /**
- * 享元模式（Flyweight Pattern）主要用于减少创建对象的数量，以减少内存占用和提高性能。
+ * 享元模式（Flyweight Pattern）主要用于减少创建对象的数量，以减少内存占用和提高性能
  * 这种类型的设计模式属于结构型模式，它提供了减少对象数量从而改善应用所需的对象结构的方式。
  */
 public class App {
