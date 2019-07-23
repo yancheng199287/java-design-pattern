d841f5fac5e72e9aad6adac41d411df0655a1dd2
hexiaoming123 <17710146600@163.com>
2019-07-23 15:23:24 +0800

    新增 策略模式

---
+++ 22-strategy/src/com/oneinlet/Strategy.java
@@ -0,0 +1,15 @@
+package com.oneinlet;
+
+/**
+ * description: 策略接口
+ *
+ * @author: dawn.he
+ * QQ:       905845006
+ * @email: dawn.he@cloudwise.com
+ * @email: 905845006@qq.com
+ * @date: 2019/7/23    3:12 PM
+ */
+public interface Strategy {
+
+   void operation();
+}
